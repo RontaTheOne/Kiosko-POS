0f76171e0a3bfeb10b3ae9b164924f44b87274b8
Ronald Tapias <ronta067@gmail.com>
2026-04-24 23:16:04 -0500

    feat: init structure

--- KioskDesign/src/App.jsx
+++ KioskDesign/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';  
 import Home from './pages/home.jsx';
+import Checkout from './pages/checkout.jsx';
 import { Navigate } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Checkout" element={<Checkout />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
---
+++ KioskDesign/src/pages/checkout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Checkout() {
+  return (
+    <div className="container">
+      <h1>Checkout</h1>
+      <p className="text-muted small"><i class="bi bi-fork-knife"></i>comer_aqui</p>
+      <p>Welcome to the Checkout page!</p>
+    </div>
+  );
+}
+export default Checkout;
